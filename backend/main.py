fa8b1391a380bd08dc41adfb68db6eb78f5b33d8
21120372 <nguyenhungyen0000@gmail.com>
2025-05-29 19:40:57 +0700

    fix run api backend

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import trends, script, tts, video, user_videos
+from routers import trends, scripts, tts, video, user_video
 import config
-
+    
 app = FastAPI(title="AI Short Video Creator API")
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 # Bao gồm các router
 app.include_router(trends.router, prefix="/trends", tags=["Trends"])
-app.include_router(script.router, prefix="/script", tags=["Script"])
+app.include_router(scripts.router, prefix="/script", tags=["Script"])
 app.include_router(tts.router, prefix="/tts", tags=["TTS"])
 app.include_router(video.router, prefix="/video", tags=["Video"])
-app.include_router(user_videos.router, prefix="/videos", tags=["User Videos"])
+app.include_router(user_video.router, prefix="/videos", tags=["User Videos"])
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to AI Short Video Creator API"}
+
+
