1e73a71e694b53d0d22fb167c7c9cf4ca2d2d1a3
davidnguyen35 <tringuyen.thirtyfive@gmail.com>
2025-06-17 21:36:05 +0700

    Minor changes

--- frontend/src/components/a2_NavBar/NavBar.js
+++ frontend/src/components/a2_NavBar/NavBar.js
@@ -5,26 +5,12 @@ import Logo from "../../assets/logo.svg"; // Import logo if needed
 const Navbar = () => {
   return (
     <nav className="navbar-bg flex justify-between items-center px-6 py-4">
-// <<<<<<< feature/home_page
-//       <div style={{display: 'flex', alignItems: 'center', gap: '12px'}}>
-//            <img src={Logo} alt="Convease Logo" style={{ height: '40px' }} />
-//       <div className="text-black text-2xl">
-//         Convease
-//       </div>
-// =======
-      <div className="flex items-center">
+      <div style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
         <img src={Logo} alt="Convease Logo" style={{ height: '40px' }} />
-        <div className="text-white text-2xl ml-3">
+        <div className="text-black text-2xl">
           Convease
         </div>
       </div>
-      {/* <div>
-        <img
-          className="avatar rounded-full w-10 h-10"
-          src="https://i.pravatar.cc/40" // ảnh avatar mẫu
-          alt="User Avatar"
-        />
-      </div> */}
     </nav>
   );
 };
