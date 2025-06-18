07694a2b3df6365d9ef924837842c114f5dd2f4c
quanphantien <22120369@student.hcmus.edu.vn>
2025-06-18 10:38:34 +0700

    fix conflict

--- frontend/src/components/a2_NavBar/NavBar.js
+++ frontend/src/components/a2_NavBar/NavBar.js
@@ -5,13 +5,11 @@ import Logo from "../../assets/logo.svg"; // Import logo if needed
 const Navbar = () => {
   return (
     <nav className="navbar-bg flex justify-between items-center px-6 py-4">
-// <<<<<<< feature/home_page
-//       <div style={{display: 'flex', alignItems: 'center', gap: '12px'}}>
+{/* //       <div style={{display: 'flex', alignItems: 'center', gap: '12px'}}>
 //            <img src={Logo} alt="Convease Logo" style={{ height: '40px' }} />
 //       <div className="text-black text-2xl">
 //         Convease
-//       </div>
-// =======
+//       </div> */}
       <div className="flex items-center">
         <img src={Logo} alt="Convease Logo" style={{ height: '40px' }} />
         <div className="text-white text-2xl ml-3">
