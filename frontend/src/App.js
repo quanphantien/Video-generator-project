6bf75795d9704b4cefae73ca8090d5c348707e08
davidnguyen35 <tringuyen.thirtyfive@gmail.com>
2025-07-01 06:02:04 +0700

    Minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,30 @@
 import React from "react";
 import { useEffect } from "react";
 import { AuthProvider } from './context/authContext';
+import ProtectedRoute from './components/ProtectedRoute';
 import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Homepage from "./components/a1_Homepage/Homepage";
 import Navbar from "./components/a2_NavBar/NavBar";
 import Footer from "./components/a3_Footer/Footer";
 import Login from "./components/a4_Login/Login";
-
 import Dashboard from "./components/b_Dashboard/Dashboard";
 import Sidebar from "./components/b_Sidebar/Sidebar";
 import Statistics from "./components/b_Statistics/Statistics";
-
 import VideoCreator from "./components/b0_VideoCreator/VideoCreator";
 import AITalkWebsite from "./pages/AITalkWebsite";
 import VideoGenerationInterface from "./pages/VideoGenerationInterface";
-
 import CreativeEditorSDKComponent from "./pages/CreativeEdititorSdk";
-
 import VideoEditor from "./components/b4_VideoEdit/VideoEdit";
 
+// // SpecialLayout.js
+// const HomePageLayout = ({ children }) => (
+//   <>
+//     <div>{children}</div>
+//   </>
+// );
 
-
-// SpecialLayout.js
-const HomePageLayout = ({ children }) => (
-  <>
-    <div>{children}</div>
-  </>
-);
 function App() {
-  // useEffect(() => {
-  //   window.addEventListener('popstate', () => {
-  //     window.location.reload();
-  //   });
-
-  //   return () => {
-  //     window.removeEventListener('popstate', () => {
-  //       window.location.reload();
-  //     });
-  //   };
-  // }, []);
-
-
   return (
     <AuthProvider>
       <Router>
@@ -58,8 +41,22 @@ function App() {
                 } />
 
                 <Route element={<MainLayout />}>
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route
+                    path="/login"
+                    element={
+                      <ProtectedRoute requireAuth={false}>
+                        <Login />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="/dashboard"
+                    element={
+                      <ProtectedRoute requireAuth={true}>
+                        <Dashboard />
+                      </ProtectedRoute>
+                    }
+                  />
                   <Route path="/statistics" element={<Statistics />} />
                   <Route path="/create" element={<VideoCreator />} />
 
