8d91c75b64ec421c81eb2a0b10c6f25a81c28eb5
21120372 <nguyenhungyen0000@gmail.com>
2025-07-01 20:50:40 +0700

    standard api response

--- backend/routers/scripts_route.py
+++ backend/routers/scripts_route.py
@@ -1,12 +1,14 @@
 import datetime
 from fastapi import APIRouter, Depends
 
+from app_code import AppCode
 from dto.script_dto import ScriptRequest, ScriptResponse
+from dto.standard_response import StandardResponse
 from services.script_service import generate
 
 router = APIRouter()
 
-@router.post("/generate", response_model=ScriptResponse)
+@router.post("/generate", response_model=StandardResponse[ScriptResponse])
 async def generate_script_endpoint(request: ScriptRequest):
     """
     Endpoint to generate a script based on the provided request data.
@@ -17,4 +19,7 @@ async def generate_script_endpoint(request: ScriptRequest):
     Returns:
         ScriptResponse: The generated script response.
     """
-    return generate(request)
+    return  StandardResponse(
+                code = AppCode.SUCCESS , 
+                message = "Generate Script successfully" ,
+                data = generate(request))
